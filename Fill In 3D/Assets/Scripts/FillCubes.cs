34def3239d65b6ef5561066ad4ec710b4b9b5524
nil17593 <navgekar18@gmail.com>
2021-10-19 23:45:19 +0530

    fixed bug of collision.

--- Fill In 3D/Assets/Scripts/FillCubes.cs
+++ Fill In 3D/Assets/Scripts/FillCubes.cs
@@ -6,16 +6,5 @@ namespace Epsilon.FillIn3D
     public class FillCubes : MonoBehaviour
     {
 
-        // Use this for initialization
-        void Start()
-        {
-
-        }
-
-        // Update is called once per frame
-        void Update()
-        {
-
-        }
     }
 }
---
+++ Fill In 3D/Assets/Scripts/GameManager.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using UnityEngine;
+
+namespace Epsilon.FillIn3D
+{
+    public class GameManager : MonoBehaviour
+    {
+        private static GameManager instance;
+        public static GameManager Instance { get { return instance; } }
+
+        [SerializeField] private GameObject levelCompletePanel;
+
+        private void Awake()
+        {
+            instance = this;
+        }
+        public void LoadLevelWinPanel()
+        {
+            levelCompletePanel.SetActive(true);
+        }
+        
+    }
+}
